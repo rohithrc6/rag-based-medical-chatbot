95a96b8ccfb08e88d0400f99f58175d7800d8c0b
Rohith Challa <rohithchalla@Rohiths-MacBook-Air-3.local>
2026-02-12 23:33:08 -0500

    pushing codebase to git

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="1.0.0",
+    author="Rohith Challa",
+    author_email="rohithrc6@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
